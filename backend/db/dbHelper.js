417f15a9bdcc4d8d1ca4e5d19c894f5aab1a96b0
Brian G <brian.gregorius@gmail.com>
2019-09-23 22:01:10 -0400

    added routes and dbhelper

---
+++ backend/db/dbHelper.js
@@ -0,0 +1,4 @@
+const Notes = require("../models/notes-model");
+const db = require("./config");
+
+module.exports = {};
--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -1,4 +1,30 @@
 const express = require("express");
 const router = express.Router();
 
+router.get("/notes", async (req, res) => {});
+
+router.post("/notes/add", async (req, res) => {
+  try {
+    const { title, body } = req.body;
+  } catch (err) {
+    res.status(500).json({ message: err });
+  }
+});
+
+router.put("/notes/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+  } catch (err) {
+    res.status(500).json({ message: err });
+  }
+});
+
+router.delete("/notes/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+  } catch (err) {
+    res.status(501).json({ message: err });
+  }
+});
+
 module.exports = router;
