3c6dbbcd5ae14b99f990f1abc429ee9c0c434eaf
Brian G <brian.gregorius@gmail.com>
2019-09-24 19:51:05 -0400

    fixed mongoose config

--- backend/db/config.js
+++ backend/db/config.js
@@ -3,4 +3,15 @@ require("dotenv").config();
 const url = process.env.MONGO_URL;
 
 const mongoose = require("mongoose");
-mongoose.connect(url, { useNewUrlParser: true });
+// mongoose.connect(url, { useNewUrlParser: true }, { useUnifiedTopology: true });
+const options = {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true
+};
+
+try {
+  mongoose.connect(url, options);
+} catch (error) {
+  handleError(error);
+}
