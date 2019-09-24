a054fadfe3c6f8f59bfd12498dea629419751a34
Brian G <brian.gregorius@gmail.com>
2019-09-23 21:39:38 -0400

    server up and running

---
+++ backend/api/server.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const configureMiddleware = require("../config/middleware.js");
+
+const server = express();
+
+configureMiddleware(server);
+
+module.exports = server;
