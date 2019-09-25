0010eb4e969799fb20dd6ba32a593d6ca13ed402
Brian G <brian.gregorius@gmail.com>
2019-09-25 11:58:17 -0400

    added logger middleware and setup redux on index

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,5 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import reducer from "./reducers/index";
+import middleware from "./middleware/index";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const store = createStore(reducer, middleware);
+console.log("state", store.getState());
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
